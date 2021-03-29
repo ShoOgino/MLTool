cb0d9c82d5e0c4c8570e5212001de7873de77b68
ShoOgino <loginologist@gmail.com>
2021-03-30 05:33:37 +0900

    modify example

--- actions/action_example.py
+++ actions/action_example.py
@@ -6,19 +6,18 @@ import re
 
 option={
     "purpose"                    : ["searchHyperParameter", "searchParameter", "test"], #set of "searchHyperParameter", "searchParameter" and "test"
-    "time2searchHyperParameter"  : 30,    # required if purpose contains "searchHyperParameter"
+    "time2searchHyperParameter"  : 60,    # required if purpose contains "searchHyperParameter"
     "modelAlgorithm"             : "DNN",  # RF or DNN
     "processor"                  : "GPU", # CPU or GPU
     "pathDatasetDir"             : "egit/isBuggy/4", # MLTool/datasets/${pathDatasetDir}
     "pathHyperParameter"         : "",    # required if purpose is ["searchParameter"]
-    "pathParameter"              : "",    # required if purpose is ["test"]
-    "date"                       : datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    "pathParameter"              : ""    # required if purpose is ["test"]
 }
 idAction = \
         option["pathDatasetDir"].replace("/","_") + "_" \
         + option["modelAlgorithm"] + "_" \
         + str(option["purpose"]) + "_" \
-        + option["date"]
+        + datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 
 print(json.dumps(option,indent=4))
 print(idAction)
